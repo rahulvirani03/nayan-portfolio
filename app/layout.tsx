631455b5533dfc82d02f64863041f8ac4adb71b9
Rahul Virani <rahul.virani@napses.com>
2025-01-05 13:47:59 +0530

    changes to meta information

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,22 +1,22 @@
-import './globals.css'
-import { Inter } from 'next/font/google'
+import "./globals.css";
+import { Inter } from "next/font/google";
 
-const inter = Inter({ subsets: ['latin'] })
+const inter = Inter({ subsets: ["latin"] });
 
 export const metadata = {
-  title: 'John Doe - Architect & Artist',
-  description: 'Portfolio of John Doe, an architect and artist specializing in modern design.',
-}
+  title: "Nayan Aher - Architect",
+  description:
+    "Portfolio of Nayan Aher, an architect specializing in modern design.",
+};
 
 export default function RootLayout({
   children,
 }: {
-  children: React.ReactNode
+  children: React.ReactNode;
 }) {
   return (
     <html lang="en">
       <body className={inter.className}>{children}</body>
     </html>
-  )
+  );
 }
-
